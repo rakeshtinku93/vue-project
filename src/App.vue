c9590336dde6066c18550df8a27bee664853f958
rakeshtinku93 <rakeshtinku93@gmail.com>
2025-04-13 11:29:33 -0400

    feat: (US) commit for adding navbar,cards component sections

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
+<script lang="ts" setup>
+import NavBarSection from '@/components/NavbarSection.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import HomeCards from '@/components/HomeCards.vue'
+</script>
 
+<template>
+  <NavBarSection />
+  <HeroSection />
+  <HomeCards />
+</template>
---
+++ src/components/JobCard.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  background: {
+    type: String,
+    default: 'bg-gray-100',
+  },
+})
+</script>
+
+<template>
+  <div :class="`${background} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
